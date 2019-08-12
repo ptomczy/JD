94e6fe7e1988224b4fad196b61a46e799389ed20
ptomczy <pawel.tomczyk@outlook.com>
2019-08-12 14:40:45 +0200

    20190812_EOD

--- jd/src/immut/pages/immut.ts
+++ jd/src/immut/pages/immut.ts
@@ -1,12 +1,42 @@
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 
+interface IObiektNumber {
+    readonly[n: number]: number;
+}
 @Component({
     selector: 'immut',
     templateUrl: 'immut.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImmutPage{
+
+    private obiekt: {
+        readonly bar: number,
+        readonly name: string
+    } = { bar: 123, name: 'MyName' };
+
     constructor(){
         console.log('CDS info: ', ChangeDetectionStrategy);
     }
+
+    iMutateFoo(obj: { bar: number, name: string }) {
+        this.obiekt.bar = obj.bar;
+        this.obiekt.name = obj.name;
+    }
+    
+    readonlyClick(){
+        this.iMutateFoo({bar: 800, name: 'Mutated name'}); 
+        console.log('Takie przychodzi foo: ', this.obiekt);
+
+        let otherObiekt: IObiektNumber = {1: 25, 2: 50};
+        
+        console.log("Takie idzie przy czytaniu: ", otherObiekt[1]);
+        //tu będę próbował zmienić właściwość otherObiektu
+        otherObiekt[0] = 12; //kompilator zwraca błąd
+        console.log("Takie dostaję po próbie przypisania właściwości dla pierwszego elementu otherObiektu: ", otherObiekt[0]);
+
+        
+    }
+
+    
 }
