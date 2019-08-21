7f735407560eed765615936d80c6bac4cc1d0d8e
ptomczy <pawel.tomczyk@outlook.com>
2019-08-21 15:04:38 +0200

    20190821_EOD

--- jd/src/rxjs/rxjs-operators/operators.ts
+++ jd/src/rxjs/rxjs-operators/operators.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { timer, combineLatest, from } from 'rxjs';
-import { map, switchMap, flatMap } from 'rxjs/operators';
+import { timer, combineLatest, from, interval, of, Observable } from 'rxjs';
+import { map, switchMap, flatMap, tap, delay, timeInterval, concatMap } from 'rxjs/operators';
 
 const t1$ = timer(1, 500);
 const t2$ = timer(1, 700);
@@ -13,6 +13,8 @@ const cars = [
     {producer: 'Opel', models: ['Adam', 'Karl', 'Corsa', 'Meriva', 'Astra', 'Insignia', 'Mokka', 'Grandland', 'Crossland']}
 ];
 
+const tapBase: Array<string> = ['Krzychu', 'Grzesiek', 'Michał', 'Alek', 'Rafał', 'Jarek'];
+
 @Component({
     selector: 'rxjs-operators',
     templateUrl: 'operators.html'
@@ -23,10 +25,12 @@ export class RxjsOperatorsComponent implements OnInit, OnDestroy{
     private infoMap: string | number;
     private infoSwitchMap: string | number;
     private infoFlatMap: Array<string>;
+    private tapTextToDisplay: string;
 
 
     ngOnInit(){
         this.res = {r1:'...', r2: '...', r3: '...'};
+        this.tapTextToDisplay = null;
     }
 
     exampleForCombinateLatest(){
@@ -56,6 +60,84 @@ export class RxjsOperatorsComponent implements OnInit, OnDestroy{
         this.infoFlatMap = [];
         let carObservable = from(cars);
         carObservable.pipe(flatMap(model => model.models)).subscribe(x => this.infoFlatMap.push(x));//bo zwracana wartość jest observablem.
+    }
+
+    exampleForTap(){
+
+        // let source = of(tapBase).pipe(delay(3000));
+
+        // let personObservable = Observable.create(async observer => {
+        //     for (let index = 0; index < tapBase.length; index++) {
+        //         observer.next(tapBase[index]);
+        //         await new Promise(res => {
+        //             setTimeout(() => {
+        //                 res();
+        //             }, 3000);
+        //         });
+        //     }
+        // }) as Observable<string>;
+
+        let source = from(tapBase);
+
+        // source.pipe(
+        //     //delay(1000),
+        //     // concatMap(el => of(el).pipe(
+        //     //     delay(1000),
+        //     //     tap(e => console.log('Pierwszy: ', e))
+        //     //     )),
+
+        //     tap(prsn => {
+        //         this.handlePersonForTap(prsn)
+        //     }),
+        //     //delay(1000),
+        //     // concatMap(el => of(el).pipe(
+        //     //     delay(1000),
+        //     //     tap(e => console.log('Drugi: ', e))
+        //     //     )),
+
+        //     map(async p => {
+        //         this.setupTextToDisplay('Pełna nazwa to: ' + p);
+        //     }),
+        //     //delay(1000),
+        //     // concatMap(el => of(el).pipe(
+        //     //     delay(1000),
+        //     //     tap(e => console.log('Trzeci: ', e))
+        //     //     )),
+        //     tap(() => this.setupTextToDisplay('Koniec dla elementu'))   
+        // ).subscribe();
+
+        source.pipe(
+            //delay(1000),
+            tap(async el => await this.handlePersonForTap(el)),
+            // delay(1000),
+            // tap(m => this.handlePersonForTap(m)),
+            // delay(1000),
+            //tap(() => console.log('Koniec'))
+        ).subscribe();
+    }
+
+    handlePersonForTap(psn: string) {
+        let m: Observable<any>;
+        m = Observable.create(observer => {
+            setTimeout(() => {
+                observer.next(psn);
+            }, 400);
+            
+        })
+        //return this.setupTextToDisplay('Trzy pierwsze litery to: ' + psn.slice(0, 3));
+        m.subscribe(x => {return this.setupTextToDisplay('Trzy pierwsze litery to: ' + x.slice(0, 3))});
+    }
+
+    setupTextToDisplay(input: string) {
+        // setTimeout(() => {
+            if(this.tapTextToDisplay == null){
+                this.tapTextToDisplay = input + '\n';
+            } else {
+                this.tapTextToDisplay = input + '\n' +  this.tapTextToDisplay;
+            }
+        // }, 400)
+
+
     }
 
     ngOnDestroy(){
