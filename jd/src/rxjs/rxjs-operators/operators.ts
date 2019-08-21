07d7046e825f058cbe52846940b938a5f9a1c37a
ptomczy <pawel.tomczyk@outlook.com>
2019-08-21 10:23:15 +0200

    20190821_10.23

--- jd/src/rxjs/rxjs-operators/operators.ts
+++ jd/src/rxjs/rxjs-operators/operators.ts
@@ -1,11 +1,17 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { timer, combineLatest } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { timer, combineLatest, from } from 'rxjs';
+import { map, switchMap, flatMap } from 'rxjs/operators';
 
 const t1$ = timer(1, 500);
 const t2$ = timer(1, 700);
 const t3$ = timer(1, 800);
 const t4$ = timer(1, 100);
+
+const cars = [
+    {producer: 'Mercedes', models: ['A-Klasse', 'B-Klasse', 'C-Klasse', 'E-Klasse', 'S-Klasse', 'G-Klasse']},
+    {producer: 'BMW', models: ['1-er', '2-er', '3-er', '4-er', '5-er', '6-er', '7-er', '8-er']},
+    {producer: 'Opel', models: ['Adam', 'Karl', 'Corsa', 'Meriva', 'Astra', 'Insignia', 'Mokka', 'Grandland', 'Crossland']}
+];
 
 @Component({
     selector: 'rxjs-operators',
@@ -16,10 +22,11 @@ export class RxjsOperatorsComponent implements OnInit, OnDestroy{
     private res: {r1: number | string, r2: number | string, r3: number | string};
     private infoMap: string | number;
     private infoSwitchMap: string | number;
+    private infoFlatMap: Array<string>;
 
 
     ngOnInit(){
-        this.res = {r1:'...', r2: '...', r3: '...'}
+        this.res = {r1:'...', r2: '...', r3: '...'};
     }
 
     exampleForCombinateLatest(){
@@ -43,6 +50,12 @@ export class RxjsOperatorsComponent implements OnInit, OnDestroy{
 
     exampleForSwitchMap(){
         t1$.pipe(switchMap(ev => t4$)).subscribe(res => this.infoSwitchMap = res);
+    }
+
+    exampleForFlatMap(){
+        this.infoFlatMap = [];
+        let carObservable = from(cars);
+        carObservable.pipe(flatMap(model => model.models)).subscribe(x => this.infoFlatMap.push(x));//bo zwracana wartość jest observablem.
     }
 
     ngOnDestroy(){
--- jd/src/rxjs/rxjs.module.ts
+++ jd/src/rxjs/rxjs.module.ts
@@ -6,10 +6,11 @@ import { RxjsComponent } from './rxjs.component';
 import { RxjsRoutingModule } from './rxjs-routing.module';
 import { MaterialModule } from 'src/app/material.module';
 import { SharedModule } from 'src/shared/shared.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [RxjsComponent, RxjsObjectCreateComponent, RxjsOperatorsComponent, RxjsSubscriptionComponent],
-    imports: [RxjsRoutingModule, MaterialModule, SharedModule],
+    imports: [RxjsRoutingModule, MaterialModule, SharedModule, CommonModule],
     exports: [RxjsComponent, RxjsObjectCreateComponent, RxjsOperatorsComponent, RxjsSubscriptionComponent]
 })
 export class RxjsModule {
