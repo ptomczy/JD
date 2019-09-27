fea382f4263f36a52545ae3617c8f5a1f5b001c8
ptomczy <pawel.tomczyk@outlook.com>
2019-09-27 10:31:27 +0200

    Animations_Ready

--- jd/src/animations/pages/animations.ts
+++ jd/src/animations/pages/animations.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -44,25 +44,29 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
             ])
         ]),
         trigger('objVisible', [
-            state('in', style({
-                opacity: 1,
-                transform: 'translateX(0)'
-            })),
-            state('highlighted', style({
-                backgroundColor: 'blue',
-                transform: 'translateX(100px)'
-            })),
             transition('void => *', [
-                style({
-                    opacity: 0,
-                    transform: 'translateX(-100px)'
-                }),
-                animate(800)
-            ]), //void jak jeszcze go nie ma w DOM
+                animate(500, keyframes([
+                    style({
+                        transform: 'translateX(-100px)',
+                        opacity: 0,
+                        offset: 0
+                    }),
+                    style({
+                        transform: 'translateX(-60px)',
+                        opacity: 0.5,
+                        offset: 0.8
+                    }),
+                    style({
+                        transform: 'translateX(0px)',
+                        opacity: 1,
+                        offset: 1
+                    })
+                ]))
+            ]),
             transition('* => void', [
-                animate(800),
+                animate(500),
                 style({
-                    transform: 'translateX(100px)',
+                    transform: 'translateX(100px) scale(2)',
                     opacity: 0
                 })
             ])
@@ -111,5 +115,9 @@ export class AngularAnimationsPage implements OnInit{
     removeItem(itm: string){
         let res = this.objectsNamesToBeAnimated.findIndex(x => x == itm);
         this.objectsNamesToBeAnimated.splice(res, 1);
+    }
+
+    animationDone(){
+        console.log("Animacja zakończona");
     }
 }
